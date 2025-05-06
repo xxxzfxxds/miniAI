2dbc715117a9f1ca34fd5b5a4d0c1e086e76b1ae
xxxzfxxds <154433713+xxxzfxxds@users.noreply.github.com>
2025-05-06 19:01:25 +0300

    Delete logger.php

--- logger.php
+++
@@ -1,6 +0,0 @@
-<?php  
-$ip = $_SERVER['REMOTE_ADDR'];  
-$referer = $_SERVER['HTTP_REFERER'] ?? 'прямой заход';  
-$data = date('Y-m-d H:i:s') . " | IP: $ip | URL: " . $_SERVER['REQUEST_URI'] . " | Источник: $referer\n";  
-file_put_contents('/var/log/secret_ips.log', $data, FILE_APPEND);  
-?>  
